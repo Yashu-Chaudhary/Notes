0fed4cb5bcf3d260df17b60dac342d4362350f91
Yashu Chaudhary <yashusky232@gmail.com>
2024-07-30 18:43:34 +0530

    completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
 
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    List<String> notes = [];
+  State<Home> createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  TextEditingController controller = TextEditingController();
+  List<String> notes = [];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Write Your Note Here'),
+        title: const Text(
+          'Notes',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12.0),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
         child: Column(
           children: [
             TextField(
-              decoration: ,
+              controller: controller,
+              decoration: const InputDecoration(
+                hintText: 'Write Your Note Here',
+                border: OutlineInputBorder(borderSide: BorderSide(width: 2)),
+              ),
+              onEditingComplete: () {
+                setState(() {
+                  notes.add(controller.text);
+                  controller.clear();
+                });
+              },
             ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ...List.generate(
+                    notes.length,
+                    (index) => Card(
+                      color: Colors.amber,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          notes[index],
+                          style: const TextStyle(fontSize: 24),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
